b6a0d5ee8fdd525db33558fff111e602e4536d04
Jansen,Sam S.J <sam.jansen@student.fontys.nl>
2016-03-22 18:08:06 +0100

    IOHelper commit

--- src/main/java/io/IoHelper.java
+++ src/main/java/io/IoHelper.java
@@ -1,5 +1,6 @@
 package io;
 
+import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -7,31 +8,80 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import domain.CarTracker;
 
 /**
- * Created by Sam on 22-3-2016.
+ * Class for saving and loading simulation data to a file.
  */
-public class IoHelper {
+public class IOHelper {
 
-    public static void serialize(List<CarTracker> carTrackers) throws IOException, ClassNotFoundException {
-        ObjectOutputStream oos = new ObjectOutputStream(
-                new FileOutputStream(getFile()));
-        oos.writeObject(carTrackers);
-        oos.close();
+    /**
+     * File to save to, and retrieve data from.
+     */
+    private static final String FILE = "trackers.tmp";
+
+    /**
+     * Save a list of carTrackers to a file.
+     *
+     * @param carTrackers to save.
+     */
+    public static void serialize(List<CarTracker> carTrackers) {
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream(getFile()));
+            oos.writeObject(carTrackers);
+        } catch (IOException ex) {
+            Logger.getLogger(IOHelper.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            close(oos);
+        }
     }
 
+    /**
+     * Load previously used saved carTrackers from a file.
+     *
+     * @return a list of carTrackers.
+     * @throws IOException            to be thrown.
+     * @throws ClassNotFoundException to be thrown.
+     */
+    @SuppressWarnings("unchecked")
     public static List<CarTracker> deserialize() throws IOException, ClassNotFoundException {
-        ObjectInputStream ois = new ObjectInputStream(
-                new FileInputStream(getFile()));
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(getFile()));
         List<CarTracker> carTrackers = (List<CarTracker>) ois.readObject();
-        ois.close();
+        close(ois);
 
         return carTrackers;
     }
 
+    /**
+     * @return true if previous simulation data is available to use.
+     */
+    public static boolean previousDataAvailable() {
+        return getFile().exists();
+    }
+
+    /**
+     * Close a object implementing the closeable interface.
+     *
+     * @param closeable object to close.
+     */
+    private static void close(Closeable closeable) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (IOException ex) {
+                Logger.getLogger(IOHelper.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    /**
+     * @return the file to save and load data from.
+     */
     private static File getFile() {
-        return new File("trackers.tmp");
+        return new File(FILE);
     }
 }
